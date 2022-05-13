1ec5d2b6a98408e1da34bb1a16ddbe70286283fc
Juan Pablo <juancho27392@hotmail.com>
2022-05-12 20:38:54 -0500

    Agregar archivos de proyecto.

---
+++ CarritoCompras/CarritoCompras/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarritoCompras
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://thefit-2e25a-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyDtg4s1z0gURWgHV_Ws5NMGtPim9X6zasc";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
